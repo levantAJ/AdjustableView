788bbccff4d77922ea9cf449bd96174b47a29aaf
levantAJ <sirlevantai@gmail.com>
2020-10-18 11:42:27 +0700

    Create project

---
+++ AdjustableView/AdjustableView.h
@@ -0,0 +1,18 @@
+//
+//  AdjustableView.h
+//  AdjustableView
+//
+//  Created by Tai Le on 10/18/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdjustableView.
+FOUNDATION_EXPORT double AdjustableViewVersionNumber;
+
+//! Project version string for AdjustableView.
+FOUNDATION_EXPORT const unsigned char AdjustableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustableView/PublicHeader.h>
+
+
